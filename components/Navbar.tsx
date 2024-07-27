9231379c33af64fd0c57e125f20392475db80720
akash-panthri <akashpanthri2000@gmail.com>
2024-07-27 07:21:26 +0530

    Added basic code to Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,24 @@
+import Link from "next/link";
+import Image from "next/image";
 
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <header className="w-full">
+      <nav className="nav">
+        <Link href="/" className="flex items-center gap-1">
+          <Image
+            src="/assets/icons/logo.svg"
+            width={27}
+            height={27}
+            alt="logo"
+          />
+          <p className="nav-logo">
+            <span className="text-primary">U</span>Price
+          </p>
+        </Link>
+        
+      </nav>
+    </header>
+  );
 }
