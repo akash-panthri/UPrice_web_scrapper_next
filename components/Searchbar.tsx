01c16dfac37101827bea80d95c3d2f5b3ed944fa
akash-panthri <akashpanthri2000@gmail.com>
2024-07-29 07:12:38 +0530

    Added basic code to Searchbar.tsx

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+"use client";
+
+import React from "react";
 
 export default function Searchbar() {
+  const handleSubmit = () => {};
   return (
-    <div>Searchbar</div>
-  )
+    <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter product link"
+        className="searchbar-input"
+      />
+      <button 
+        type="submit" 
+        className="searchbar-btn"
+      >Search</button>
+    </form>
+  );
 }
