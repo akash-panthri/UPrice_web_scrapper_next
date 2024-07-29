ccd1feeac0901e1d995e1f439feef32270433116
akash-panthri <akashpanthri2000@gmail.com>
2024-07-29 07:23:22 +0530

    Added Carousel in HeroCarousel.tsx

--- components/HeroCarousel.tsx
+++ components/HeroCarousel.tsx
@@ -1,7 +1,25 @@
-import React from 'react'
+"use client";
+
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
 export default function HeroCarousel() {
   return (
-    <div>HeroCarousel</div>
-  )
+    <div>
+      <Carousel>
+        <div>
+          <img src="assets/1.jpeg" />
+          <p className="legend">Legend 1</p>
+        </div>
+        <div>
+          <img src="assets/2.jpeg" />
+          <p className="legend">Legend 2</p>
+        </div>
+        <div>
+          <img src="assets/3.jpeg" />
+          <p className="legend">Legend 3</p>
+        </div>
+      </Carousel>
+    </div>
+  );
 }
