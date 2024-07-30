045fceda76f2884af076c5d9f70ddb416a328d65
akash-panthri <akashpanthri2000@gmail.com>
2024-07-30 07:09:18 +0530

    Added heroImages constant in HeroCarousel.tsx

--- components/HeroCarousel.tsx
+++ components/HeroCarousel.tsx
@@ -3,6 +3,13 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 
+const heroImages = [
+    { imgUrl: '/assets/images/hero-1.svg', alt: 'smartwatch'},
+    { imgUrl: '/assets/images/hero-2.svg', alt: 'bag'},
+    { imgUrl: '/assets/images/hero-3.svg', alt: 'lamp'},
+    { imgUrl: '/assets/images/hero-4.svg', alt: 'air fryer'},
+    { imgUrl: '/assets/images/hero-5.svg', alt: 'chair'},
+  ]
 export default function HeroCarousel() {
   return (
     <div>
