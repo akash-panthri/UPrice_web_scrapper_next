fea7215d88e313ce275283e9f33b8c7aa2ae640f
akash-panthri <akashpanthri2000@gmail.com>
2024-08-01 08:11:21 +0530

    Added isValidAmazonProductURL function

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -2,6 +2,21 @@
 
 import { useState, FormEvent } from "react";
 
+const isValidAmazonProductURL = (url: string) => {
+  try {
+    const parsedURL = new URL(url);
+    const hostname = parsedURL.hostname;
+
+    if (hostname.includes("amazon.") || hostname.endsWith("amazon")) {
+      return true;
+    }
+  } catch (error) {
+    return false;
+  }
+
+  return false;
+};
+
 export default function Searchbar() {
   const [searchPrompt, setSearchPrompt] = useState("");
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {};
