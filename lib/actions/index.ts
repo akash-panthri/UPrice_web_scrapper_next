a28a0925d1f31670dcf00f6a3a23f6a9205ab47a
akash-panthri <akashpanthri2000@gmail.com>
2024-08-02 07:15:43 +0530

    Added scrapeAndStoreProduct in index.ts in lib/actions

---
+++ lib/actions/index.ts
@@ -0,0 +1,13 @@
+"use server"
+
+
+export async function scrapeAndStoreProduct(productUrl: string) {
+  if(!productUrl) return;
+  try {
+    
+  } catch (error: any) {
+    throw new Error(`Failed to create/update product: ${error.message}`)
+  }
+
+
+}
