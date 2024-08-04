39f3e3b66f7202caf5167774bdcae3735e2064b4
akash-panthri <akashpanthri2000@gmail.com>
2024-08-04 07:05:19 +0530

    Set BrightData proxy configuration in scrapeAmazonProduct in index.ts in lib/scrapper again

--- lib/scrapper/index.ts
+++ lib/scrapper/index.ts
@@ -6,4 +6,22 @@
 export async function scrapeAmazonProduct(url: string) {
     if(!url) return;
 
+
+    // BrightData proxy configuration
+    const username = String(process.env.BRIGHT_DATA_USERNAME);
+    const password = String(process.env.BRIGHT_DATA_CURL_PASSWORD);
+    const port = process.env.BRIGHT_DATA_PORT;
+    const session_id = (1000000 * Math.random()) | 0;
+
+    const options = {
+        auth: {
+          username: `${username}-session-${session_id}`,
+          password,
+        },
+        host: String(process.env.BRIGHT_DATA_HOST_STRING),
+        port,
+        rejectUnauthorized: false,
+      }
+
+      
 }
