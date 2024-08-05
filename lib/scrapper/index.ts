33d9712cfe110747cec37286974c8ed8c28a5511
akash-panthri <akashpanthri2000@gmail.com>
2024-08-05 05:31:58 +0530

    Added cheerio selection  in index.ts in lib/scrapper

--- lib/scrapper/index.ts
+++ lib/scrapper/index.ts
@@ -1,40 +1,42 @@
+"use server";
 
-"use server"
-
-import axios from 'axios';
-import * as cheerio from 'cheerio';
+import axios from "axios";
+import * as cheerio from "cheerio";
+import { extractPrice } from "../utils";
 
 export async function scrapeAmazonProduct(url: string) {
-    if(!url) return;
+  if (!url) return;
 
+  // BrightData proxy configuration
+  const username = String(process.env.BRIGHT_DATA_USERNAME);
+  const password = String(process.env.BRIGHT_DATA_CURL_PASSWORD);
+  const port = process.env.BRIGHT_DATA_PORT;
+  const session_id = (1000000 * Math.random()) | 0;
 
-    // BrightData proxy configuration
-    const username = String(process.env.BRIGHT_DATA_USERNAME);
-    const password = String(process.env.BRIGHT_DATA_CURL_PASSWORD);
-    const port = process.env.BRIGHT_DATA_PORT;
-    const session_id = (1000000 * Math.random()) | 0;
+  const options = {
+    auth: {
+      username: `${username}-session-${session_id}`,
+      password,
+    },
+    host: String(process.env.BRIGHT_DATA_HOST_STRING),
+    port,
+    rejectUnauthorized: false,
+  };
 
-    const options = {
-        auth: {
-          username: `${username}-session-${session_id}`,
-          password,
-        },
-        host: String(process.env.BRIGHT_DATA_HOST_STRING),
-        port,
-        rejectUnauthorized: false,
-      }
+  try {
+    // Fetching the product page
+    const response = await axios.get(url, options);
+    const $ = cheerio.load(response.data);
 
-      try {
-        // Fetching the product page
-        const response = await axios.get(url, options);
-        const $ = cheerio.load(response.data);
-
-
-        // Extracting the product title (id)
-        const title = $('#productTitle').text().trim();
-        // const currentPrice = extractPrice()
-      } catch (error:any) {
-        throw new Error(`Failed to scrape the product: ${error.message}`)
-      }
-      
+    // Extracting the product title (id)
+    const title = $("#productTitle").text().trim();
+    const currentPrice = extractPrice(
+      $(".priceToPay span.a-price-whole"),
+      $(".a.size.base.a-color-price"),
+      $(".a-button-selected .a-color-base"),
+      $('.a-price.a-text-price'),
+    );
+  } catch (error: any) {
+    throw new Error(`Failed to scrape the product: ${error.message}`);
+  }
 }
