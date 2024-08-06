98c3cb633db2dea22d01892ee6d32b967e804a42
akash-panthri <akashpanthri2000@gmail.com>
2024-08-07 05:06:55 +0530

    Added extractCurrency function in utils.ts in li

--- lib/scrapper/index.ts
+++ lib/scrapper/index.ts
@@ -33,23 +33,29 @@ export async function scrapeAmazonProduct(url: string) {
     const currentPrice = extractPrice(
       $(".priceToPay span.a-price-whole"),
       $(".a.size.base.a-color-price"),
-      $(".a-button-selected .a-color-base"),
+      $(".a-button-selected .a-color-base")
     );
 
     const originalPrice = extractPrice(
-      $('#priceblock_ourprice'),
-      $('.a-price.a-text-price span.a-offscreen'),
-      $('#listPrice'),
-      $('#priceblock_dealprice'),
-      $('.a-size-base.a-color-price')
+      $("#priceblock_ourprice"),
+      $(".a-price.a-text-price span.a-offscreen"),
+      $("#listPrice"),
+      $("#priceblock_dealprice"),
+      $(".a-size-base.a-color-price")
     );
-   
-    const outOfStock = $('#availability span').text().trim().toLowerCase() === 'currently unavailable';
 
-    const images = 
-      $('#imgBlkFront').attr('data-a-dynamic-image') || 
-      $('#landingImage').attr('data-a-dynamic-image')
+    const outOfStock =
+      $("#availability span").text().trim().toLowerCase() ===
+      "currently unavailable";
 
+    const images =
+      $("#imgBlkFront").attr("data-a-dynamic-image") ||
+      $("#landingImage").attr("data-a-dynamic-image") ||
+      "{}";
+
+    const imageUrls = Object.keys(JSON.parse(images));
+
+    // const currency = extractCurrency($('.a-price-symbol'))
   } catch (error: any) {
     throw new Error(`Failed to scrape the product: ${error.message}`);
   }
--- lib/utils.ts
+++ lib/utils.ts
@@ -7,3 +7,10 @@ export function extractPrice(...elements: any) {
 
   return "";
 }
+
+
+// Extracts and returns the currency symbol from an element.
+export function extractCurrency(element: any) {
+  const currencyText = element.text().trim().slice(0, 1);
+  return currencyText ? currencyText : "";
+}
