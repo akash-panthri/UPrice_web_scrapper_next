3786556efc958c1b2f3259e4c15f75d7b4185428
akash-panthri <akashpanthri2000@gmail.com>
2024-08-09 06:24:11 +0530

    Added mongoose.ts in lib folder

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,11 +1,18 @@
 "use server"
 
 import { scrapeAmazonProduct } from "../scrapper";
+import { connectToDB } from "../mongoose";
 
 export async function scrapeAndStoreProduct(productUrl: string) {
   if(!productUrl) return;
   try {
+    connectToDB()
+    
     const scrapedProduct = await scrapeAmazonProduct(productUrl);
+
+    if(!scrapedProduct) return;
+ 
+    
   } catch (error: any) {
     throw new Error(`Failed to create/update product: ${error.message}`)
   }
