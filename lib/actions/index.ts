0e420cb55b344ec17c0c798a2bcd2892c9508683
akash-panthri <akashpanthri2000@gmail.com>
2024-08-10 06:32:19 +0530

    Added function getProductById in index.ts in lib/actions

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -37,11 +37,27 @@ export async function scrapeAndStoreProduct(productUrl: string) {
       product,
       { upsert: true, new: true }
     );
-    
+
+    // revalidatePath(`/products/${newProduct._id}`);
 
   } catch (error: any) {
     throw new Error(`Failed to create/update product: ${error.message}`)
   }
 
 
+}
+
+
+export async function getProductById(productId: string) {
+  try {
+    connectToDB();
+
+    const product = await Product.findOne({ _id: productId });
+
+    if(!product) return null;
+
+    return product;
+  } catch (error) {
+    console.log(error);
+  }
 }
