80b1bf03235eacfeae69448fb26eef2d396b8b45
akash-panthri <akashpanthri2000@gmail.com>
2024-08-12 06:37:45 +0530

    Added code to ProductCard.tsx in components folder

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import HeroCarousel from "@/components/HeroCarousel";
 import Searchbar from "@/components/Searchbar";
 import Image from "next/image";
 import { getAllProducts } from "@/lib/actions"
+import ProductCard from "@/components/ProductCard"
 
 const Home = async() => {
   const allProducts = await getAllProducts();
@@ -37,7 +38,7 @@ const Home = async() => {
         <h2 className="section-text">Trending</h2>
         <div className="flex flex-wrap gap-x-8 gap-y-16">
           {allProducts?.map((product) => (
-            <div>{product.title}</div>
+            <ProductCard key={product._id} product={product} />
           ))}
         </div>
       </section>
